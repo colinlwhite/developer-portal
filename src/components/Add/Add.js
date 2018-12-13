790b59a86b82742fdd7cd47755fe0420b254fa0f
Colin White <clwhite814@gmail.com>
2018-12-12 20:44:28 -0600

    Auth works and printing h2s to the page

---
+++ src/components/Add/Add.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Add.scss';
+
+class Add extends React.Component {
+  render() {
+    return (
+      <div className="Add">
+        <h2>Add</h2>
+      </div>
+    );
+  }
+}
+
+export default Add;
---
+++ src/components/Forms/Forms.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Forms.scss';
+
+class Forms extends React.Component {
+  render() {
+    return (
+      <div className="forms">
+        <h2>Forms</h2>
+      </div>
+    );
+  }
+}
+
+export default Forms;
