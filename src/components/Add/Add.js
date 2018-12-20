a0d3a369848d5d421c61cddd85af9f0f9320c464
Colin White <clwhite814@gmail.com>
2018-12-20 14:16:47 -0600

    added the form

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -6,6 +6,19 @@ class Add extends React.Component {
     return (
       <div className="Add col">
         <h2>Add Learning Material</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="resource">Resource:</label>
+            <input 
+              type="text"
+              className="form-control"
+              id="resource"
+              aria-describedby="addressHelp"
+              placeholder="Add Resource Here"
+            />
+          </div>
+          <button className="btn btn-danger">Save Resource</button>
+        </form>
       </div>
     );
   }
