eb7f73b16225b3540eacd0b00a22fd667bf4969c
Colin White <clwhite814@gmail.com>
2018-12-20 15:33:31 -0600

    the onchange works

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import './Add.scss';
 
+const defaultListing = {
+  name: '',
+  link: '',
+  category: '',
+  uid: '',
+};
+
 class Add extends React.Component {
+  state = {
+    newResource: defaultListing,
+  }
+
+  formFieldStringState = (name, e) => {
+    e.preventDefault();
+    const tempListing = { ...this.state.newResource };
+    tempListing[name] = e.target.value;
+    this.setState({ newResource: tempListing });
+  }
+
+  resourceChange = e => this.formFieldStringState('name', e);
+
   render() {
+    const { newResource } = this.state;
     return (
       <div className="Add col">
         <h2>Add Learning Material</h2>
         <form>
           <div className="form-group">
             <label htmlFor="resource">Resource:</label>
-            <input 
+            <input
               type="text"
               className="form-control"
               id="resource"
-              aria-describedby="addressHelp"
+              aria-describedby="resourceHelp"
               placeholder="Add Resource Here"
+              value={newResource.name}
+              onChange={this.resourceChange}
             />
           </div>
           <button className="btn btn-danger">Save Resource</button>
